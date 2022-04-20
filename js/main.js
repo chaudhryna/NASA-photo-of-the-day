a82529de0918edb7483032dee0cba06a4db6c92f
Nancy Chaudhry <nancych14@gmail.com>
2022-04-20 11:54:24 +0500

    Changed app to search random dates

--- js/main.js
+++ js/main.js
@@ -5,13 +5,21 @@ const token = config.MY_API_TOKEN
 document.querySelector('button').addEventListener('click', getPic)
 
 function getPic() {
-  const searchDate = document.querySelector('input').value
+  // const searchDate = document.querySelector('input').value
+
+  function randomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  }
+
+  const searchDate = randomDate(new Date(1995, 06, 16), new Date());
+  console.log(d);
+
   const url = `https://api.nasa.gov/planetary/apod?api_key=${token
 }=${searchDate}`
 
-  if (searchDate < '1995-06-16') {
-    document.querySelector('h2').innerText = 'No picture found for that date. Photo requests must be after 06/16/1995'
-  } else {
+  // if (searchDate < '1995-06-16') {
+  //   document.querySelector('h2').innerText = 'No picture found for that date. Photo requests must be after 06/16/1995'
+  // } else {
 
   fetch(url)
     .then(res => res.json()) // parse response as JSON
@@ -30,5 +38,4 @@ function getPic() {
       .catch(err => {
         console.log(`error ${err}`)
       })
-  }
 }
